b00096f31081373899f5cf7170ffd9a6d6f84171
Aditya Raghav <aditya.raghv442@gmail.com>
2026-04-29 12:33:31 +0530

    Create maximum product subarray.java

---
+++ Kadane/maximum product subarray.java
@@ -0,0 +1,18 @@
+// TC O(n);
+// SC O(1);
+class Solution {
+    public int maxProduct(int[] nums) {
+        int minEnding = nums[0];
+        int maxEnding = nums[0];
+        int result = nums[0];
+        for(int i = 1; i<nums.length; i++){
+            int v1 = nums[i];
+            int v2 = minEnding * nums[i];
+            int v3 = maxEnding * nums[i];
+            maxEnding = Math.max(v1,Math.max(v2,v3));
+            minEnding = Math.min(v1,Math.min(v2,v3));
+            result = Math.max(result,Math.max(maxEnding,minEnding));
+        }
+        return result;
+    }
+}
