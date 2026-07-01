437205c8cc9533268fbb7adaadc4280f14b8383a
Aditya Raghav <aditya.raghv442@gmail.com>
2026-07-01 13:20:04 +0530

    Update PalindromeNumber.java

--- Recursion/PalindromeNumber.java
+++ Recursion/PalindromeNumber.java
@@ -1,25 +1,14 @@
-public class PalindromeNumber {
-    static boolean checkPalindrome2(int num, int reverse, int copy){
-        if(num==0){
+class Solution {
+    static boolean isPalindrome(int num, int reverse, int copy){
+        if(num == 0){
             return reverse == copy;
         }
         int lastDigit = num % 10;
         reverse = reverse * 10 + lastDigit;
-        return checkPalindrome2(num / 10, reverse, copy);
+        return isPalindrome(num/10,reverse,copy);
     }
-    static void checkPalindrome(int num, int reverse, int copy){
-        if(num==0){
-            System.out.println(reverse == copy?"Palindrome ":"Not Palindrome");
-            return;
-        }
-        int lastDigit = num % 10;
-        reverse = reverse * 10 + lastDigit;
-        checkPalindrome(num / 10, reverse, copy);
-    }
-    public static void main(String[] args) {
-        int num = 321;
-        int copy = num;
-        boolean r = checkPalindrome2(num, 0, copy);
-        System.out.println(r?"Palindrome ":"Not Palindrome");
+    public boolean isPalindrome(int x) {
+        if(x<0) return false;
+        return isPalindrome(x,0,x);
     }
 }
